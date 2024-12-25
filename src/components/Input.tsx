05ae819c65a664c1e40b5cd0f0ea841c455bd7d2
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-12-26 01:26:46 +0530

    added socket

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,15 +3,22 @@ import { wsState } from "../atoms"
 import { useRef } from "react"
 
 export const Input = () =>{
-    const inputRef = useRef()
+    const inputRef = useRef<HTMLInputElement>(null)
     const wsVal = useRecoilValue(wsState)
     const handleSendMessage = () =>{
-        
+
+        wsVal?.send(JSON.stringify({
+            type:"chat",
+            payload:{
+                message:inputRef.current?.value
+            }
+        }))
     }
     return(
         <>  
             <div className="flex mt-2">
-                <input type="text" placeholder="Enter a Message..." className="w-4/5 h-12 bg-black ml-4 text-white  border border-[#D3D3D3] border-opacity-40 rounded-lg"/>
+                <input ref={inputRef}
+                 type="text" placeholder="Enter a Message..." className="w-4/5 h-12 bg-black ml-4 text-white  border border-[#D3D3D3] border-opacity-40 rounded-lg"/>
                 <button onClick={handleSendMessage}
                 className="bg-black text-white px-4 py-2 rounded-lg  border border-[#D3D3D3] border-opacity-40 ml-4">
                     Send
