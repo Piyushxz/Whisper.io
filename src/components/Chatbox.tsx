f4ac7572f5328b6a1e2c15df2318e213accf189a
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-02 02:14:12 +0530

    css issue

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -4,7 +4,7 @@ import { Input } from "./Input"
 export const Chatbox = () =>{
     return(
         <>
-            <div className="w-[600px]  h-[600px] lg:h-[1150px] xl:h-[650px] bg-[#191919] shadow-inner rounded-lg border border-[#D3D3D3] border-opacity-40 ">
+            <div className="w-[600px] h-[800px] lg:h-[1150px] xl:h-[650px] bg-[#191919] shadow-inner rounded-lg border border-[#D3D3D3] border-opacity-40 ">
                 <div className="flex flex-col h-full ">
                     <div className="h-[90vh] border-b border-[#D3D3D3] border-opacity-40  ">
                         <Chats/>
